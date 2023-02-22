0ee358e49cb62d22b86bb84a73bfb96c9dcaabc6
michazaxm <76849178+michazaxm@users.noreply.github.com>
2023-02-22 13:36:45 +0700

    Update shifrator.py

--- shifrator.py
+++ shifrator.py
@@ -1,4 +1,5 @@
-from random import seed, shuffle
+from random import seed, shuffle, randbytes
+from operator import xor
 
 class BitArray:
 	def __init__(self, _bytes: bytes) -> None:
@@ -12,6 +13,10 @@ class BitArray:
 		return bytes(int(''.join(str(bit) for bit in bits), 2) for bits in [self.data[i:i + 8] for i in range(0, len(self.data), 8)])
 	def __str__(self) -> str:
 		return str([self.data[i:i + 8] for i in range(0, len(self.data), 8)]).replace(', ', '').replace('[', ' ').replace(']', '')[2:]
+
+	def _genSeed(self, password: str) -> int:
+		return sum([ord(x)**2 for x in password])
+
 	def bitEncode(self, password):
 		mask = list(range(len(self.data)))
 		seed(password)
@@ -30,4 +35,15 @@ class BitArray:
 		for d in mask:
 			result.append(self.data[d])
 		self.data = result
+		return self
+	
+	def xorEncode(self, key=b''):
+		byts = self.to_bytes()
+		mask = randbytes(len(byts))
+		enc = bytes([xor(s, mask[i]) for (i, s) in enumerate(byts)])
+		self.data = self.to_bits(enc)
+		return mask
+	def xorDecode(self, key):
+		data = bytes([xor(s, key[i]) for (i, s) in enumerate(self.to_bytes())])
+		self.data = self.to_bits(data)
 		return self
